6f8355012e9d9ca118299e8203e44d73431d9eee
linnefromice <linnefromice@gmail.com>
2020-12-23 10:51:29 +0900

    minor collection

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:linnefromice/screens/list_tile_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
