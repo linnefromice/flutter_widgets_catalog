d1604dfa31cea46c27325a89423e0909a2ce6466
linnefromice <linnefromice@gmail.com>
2020-12-25 01:59:14 +0900

    update GridViewScreen (temp UI)

--- lib/screens/grid_view_screen.dart
+++ lib/screens/grid_view_screen.dart
@@ -6,20 +6,46 @@ class GridViewScreen extends StatefulWidget {
 }
 
 class _State extends State<GridViewScreen> {
-  List<Widget> _children = List.generate(10, (index) => Container(
-    color: Colors.pink[index * 100],
-    child: CircleAvatar(
-      child: Text(index.toString()),
-      backgroundColor: Colors.white,
-    ),
-  ));
+  int _selectIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
+  Widget _selectBody(int index) {
+    switch(index) {
+      case 0:
+        return _GridViewCount();
+      case 1:
+        return Center(
+          child: Text("You are selected .builder"),
+        );
+      case 2:
+        return Center(
+          child: Text("You are selected .custom"),
+        );
+      case 3:
+        return Center(
+          child: Text("You are selected .extent"),
+        );
+      default:
+        return Center(
+          child: Text("Exception."),
+        );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _GridViewCount(),
+      body: _selectBody(_selectIndex),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.pinkAccent,
+        currentIndex: _selectIndex,
+        onTap: _onItemTapped,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.looks_one),
