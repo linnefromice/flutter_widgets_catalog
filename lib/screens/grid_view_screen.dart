bb65d066575b45c9ff9d19e4b98087d629e75f1b
linnefromice <linnefromice@gmail.com>
2020-12-25 01:45:07 +0900

    separate widget

--- lib/screens/grid_view_screen.dart
+++ lib/screens/grid_view_screen.dart
@@ -17,12 +17,27 @@ class _State extends State<GridViewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        crossAxisCount: 3,
-        children: _children
-      ),
+      body: _GridViewCount()
+    );
+  }
+}
+
+class _GridViewCount extends StatelessWidget {
+  final List<Widget> _children = List.generate(10, (index) => Container(
+    color: Colors.pink[index * 100],
+    child: CircleAvatar(
+      child: Text(index.toString()),
+      backgroundColor: Colors.white,
+    ),
+  ));
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      crossAxisCount: 3,
+      children: _children
     );
   }
 }
