8432548b1d369b6eb66e27b073994254d9c9742a
linnefromice <linnefromice@gmail.com>
2020-12-27 20:31:10 +0900

    use stateful widget to TabBarViewScreen

---
+++ lib/screens/tab_bar_view_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabBarViewScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<TabBarViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
