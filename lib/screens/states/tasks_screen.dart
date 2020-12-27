391108316f875f8076639f9fe551eac15d1d2f33
linnefromice <linnefromice@gmail.com>
2020-12-28 00:51:08 +0900

    create model for tasks

--- lib/screens/states/tasks_screen.dart
+++ lib/screens/states/tasks_screen.dart
@@ -2,6 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+enum Status {
+  PENDING,
+  READY,
+  DOING,
+  DONE,
+}
+
+class Task {
+  Task({this.id, this.title, this.status, this.createDate, this.updateDate});
+  int id;
+  String title;
+  Status status;
+  String createDate;
+  String updateDate;
+}
+
 class TasksScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
