88e883189ecd579bda892c8654acd522779af3f7
linnefromice <linnefromice@gmail.com>
2020-12-28 01:05:51 +0900

    create provider & use reading state

--- lib/screens/states/tasks_screen.dart
+++ lib/screens/states/tasks_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 enum Status {
   PENDING,
@@ -10,7 +11,13 @@ enum Status {
 }
 
 class Task {
-  Task({this.id, this.title, this.status, this.createDate, this.updateDate});
+  Task(
+    this.id,
+    this.title,
+    this.status,
+    this.createDate,
+    this.updateDate
+  );
   int id;
   String title;
   Status status;
@@ -18,13 +25,36 @@ class Task {
   String updateDate;
 }
 
+final _tasksProvider = StateNotifierProvider((_) => TasksState());
+
+class TasksState extends StateNotifier<List<Task>> {
+  TasksState() : super([
+    Task(1, "DONE task", Status.DONE, "20201225", "20201225"),
+    Task(2, "DOING task", Status.DOING, "20201225", "20201225"),
+    Task(3, "READY task", Status.READY, "20201225", "20201225"),
+    Task(4, "PENDING task", Status.PENDING, "20201225", "20201225"),
+  ]);
+}
+
 class TasksScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final state = useProvider(_tasksProvider.state);
+
     return Scaffold(
-      body: Center(
-        child: Text("Tasks"),
-      ),
+      body: ListView.builder(
+        itemCount: state.length,
+        itemBuilder: (BuildContext context, int index) {
+          final task = state[index];
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.work),
+              title: Text(task.title),
+              subtitle: Text("${task.id.toString()} / ${task.createDate}"),
+            ),
+          );
+        },
+      )
     );
   }
 }
