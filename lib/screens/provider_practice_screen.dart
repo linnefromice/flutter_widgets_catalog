9d0709ad65166dc7fe0e0ab9b91e6be9fb40470b
linnefromice <linnefromice@gmail.com>
2020-12-27 22:11:32 +0900

    use ChangeNotifierProvider - for reading

--- lib/screens/provider_practice_screen.dart
+++ lib/screens/provider_practice_screen.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterModel extends ChangeNotifier {
+  int count = 0;
+
+  void increment() {
+    count++;
+    notifyListeners();
+  }
+}
 
 class ProviderPracticeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Text('0'),
-          ],
+    return ChangeNotifierProvider(
+      create: (_) => CounterModel(),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('You have pushed the button this many times:'),
+              CounterText(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CounterText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      Provider.of<CounterModel>(context).count.toString(),
+      style: Theme.of(context).textTheme.headline3,
     );
   }
 }
