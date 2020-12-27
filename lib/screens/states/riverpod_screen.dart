eb3b06f03845ef67503c7fde554c7510051bea96
linnefromice <linnefromice@gmail.com>
2020-12-27 23:27:11 +0900

    create new screen

---
+++ lib/screens/states/riverpod_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RiverpodScreen extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, T Function<T>(ProviderBase<Object, T> provider) watch) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
