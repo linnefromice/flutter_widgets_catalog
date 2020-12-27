10ce730ec6cbfae268fb963e4157253868f5652a
linnefromice <linnefromice@gmail.com>
2020-12-27 23:24:07 +0900

    update paths

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:linnefromice/screens/grid_view_screen.dart';
 import 'package:linnefromice/screens/interactive_viewer_screen.dart';
 import 'package:linnefromice/screens/list_tile_screen.dart';
-import 'package:linnefromice/screens/provider_practice_screen.dart';
+import 'package:linnefromice/screens/states/provider_practice_screen.dart';
 import 'package:linnefromice/screens/sliver_app_bar_screen.dart';
 import 'package:linnefromice/screens/tab_bar_view_screen.dart';
 
